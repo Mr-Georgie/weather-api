3cd15da1ec1bba07408928fe95c357a390def050
Mr-Georgie <george.isiguzo@yahoo.com>
2024-11-16 11:02:22 +0100

    added pr template and fix import bug

--- src/weather.graphql/test/weather.graphql.resolver.spec.ts
+++ src/weather.graphql/test/weather.graphql.resolver.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { WeatherGraphqlResolver } from './weather.graphql.resolver';
+import { Test, TestingModule } from "@nestjs/testing";
+import { WeatherGraphqlResolver } from "../weather.graphql.resolver";
 
-describe('WeatherGraphqlResolver', () => {
-  let resolver: WeatherGraphqlResolver;
+describe("WeatherGraphqlResolver", () => {
+    let resolver: WeatherGraphqlResolver;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [WeatherGraphqlResolver],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [WeatherGraphqlResolver],
+        }).compile();
 
-    resolver = module.get<WeatherGraphqlResolver>(WeatherGraphqlResolver);
-  });
+        resolver = module.get<WeatherGraphqlResolver>(WeatherGraphqlResolver);
+    });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+    it("should be defined", () => {
+        expect(resolver).toBeDefined();
+    });
 });
